12ceb61702951c9e8f76e344faef34d35b810278
brunosr3003 <70835031+brunosr3003@users.noreply.github.com>
2024-12-09 21:16:51 -0300

    usuario

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// tailwind.config.js
+import daisyui from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,7 +10,7 @@ export default {
       },
       colors: {
         primary: "#FF7B07",
-        secundary: "#1E3465", // Erro: "secundary" deveria ser "secondary"
+        secondary: "#1E3465", // Corrigido o nome da cor de "secundary" para "secondary"
       },
     },
     container: {
@@ -22,20 +22,20 @@ export default {
       maxWidth: "100%",
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: [
       {
         mytheme: {
-          "primary": "#078bff",
-          "secondary": "#1E3465",
-          "accent": "#37CDBE",
-          "neutral": "#3D4451",
+          primary: "#078bff",
+          secondary: "#1E3465",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
           "base-100": "#FFFFFF",
-          "info": "#2094f3",
-          "success": "#009485",
-          "warning": "#FF9900",
-          "error": "#FF5724",
+          info: "#2094f3",
+          success: "#009485",
+          warning: "#FF9900",
+          error: "#FF5724",
         },
       },
     ],
