bbf79794ed31a8eb048e9a543063663706ac45e2
brunosr3003 <70835031+brunosr3003@users.noreply.github.com>
2024-12-09 22:06:08 -0300

    vercelinho

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
-  },
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/v1/api': {
-        target: 'http://localhost:1000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
 });
